ad712f079554a0a5fdfcd698552795cf56130300
James Lovejoy <james.lovejoy2@gmail.com>
2019-12-28 15:02:28 -0600

    added inquirer and axios code and inspecting response object to find proper paths to user data needed

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,44 @@
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
 const questions = [
   
 ];
 
-function writeToFile(fileName, data) {
- 
+init();
+
+inquirer
+  .prompt({
+    message: "Enter your GitHub username:",
+    name: "username"
+  })
+  .then(function({ username }) {
+    const queryUrl = `https://api.github.com/users/${username}`;
+
+    axios.get(queryUrl).then(function(res) {
+        console.log(res);
+    //   const repoNames = res.data.map(function(repo) {
+    //     return repo.name;
+      });
+
+    //   const repoNamesStr = repoNames.join("\n");
+
+    //   fs.writeFile("repos.txt", repoNamesStr, function(err) {
+    //     if (err) {
+    //       throw err;
+    //     }
+
+    //     console.log(`Saved ${repoNames.length} repos`);
+    //   });
+    });
+
+
+  function writeToFile(fileName, data) {
 }
 
 function init() {
+}
 
-init();
+
+//to get number of public repos use resp.data.length
+//to get number of followers use 
