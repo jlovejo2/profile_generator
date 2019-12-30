20d7d0107366fe7f16c65a35b7a290c8de23087b
James Lovejoy <james.lovejoy2@gmail.com>
2019-12-29 20:03:21 -0600

    added cards to base html code, included resp object code to find desired info from github axios call

--- Develop/index.js
+++ Develop/index.js
@@ -2,43 +2,55 @@ const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 const questions = [
-  
+
 ];
 
 init();
 
 inquirer
-  .prompt({
-    message: "Enter your GitHub username:",
-    name: "username"
-  })
-  .then(function({ username }) {
-    const queryUrl = `https://api.github.com/users/${username}`;
+    .prompt([
 
-    axios.get(queryUrl).then(function(res) {
-        console.log(res);
-    //   const repoNames = res.data.map(function(repo) {
-    //     return repo.name;
-      });
+        {
+            message: "Enter your GitHub username:",
+            name: "username"
+        },
 
-    //   const repoNamesStr = repoNames.join("\n");
+        {
+            message: "What is your favorite color?",
+            name: "background-color"
+        }
+    ])
+    .then(function (resp) {
+        const queryUrl = `https://api.github.com/users/${resp.username}`;
 
-    //   fs.writeFile("repos.txt", repoNamesStr, function(err) {
-    //     if (err) {
-    //       throw err;
-    //     }
+        axios.get(queryUrl).then(function (res) {
+            console.log(res.data.public_repos);
+            console.log(res.data.followers);
+            console.log(res.data.following);
+            //   const repoNames = res.data.map(function(repo) {
+            //     return repo.name;
+        });
 
-    //     console.log(`Saved ${repoNames.length} repos`);
-    //   });
+        //   const repoNamesStr = repoNames.join("\n");
+
+        //   fs.writeFile("repos.txt", repoNamesStr, function(err) {
+        //     if (err) {
+        //       throw err;
+        //     }
+
+        //     console.log(`Saved ${repoNames.length} repos`);
+        //   });
     });
 
 
-  function writeToFile(fileName, data) {
+function writeToFile(fileName, data) {
 }
 
 function init() {
 }
 
 
-//to get number of public repos use resp.data.length
-//to get number of followers use 
+//to get number of public repos use resp.data.public_repos
+//to get number of followers use resp.data.followers
+//to get number of users following resp.data.following
+//to get number of github stars resp.data
